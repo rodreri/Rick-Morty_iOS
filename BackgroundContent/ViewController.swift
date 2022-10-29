a3f25e6a8eda8ef9a2ad2b3ef9d0c16a6e33552d
erick <rodreri>
2022-10-29 11:36:30 -0500

    Initial Commit

---
+++ BackgroundContent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BackgroundContent
+//
+//  Created by Erick Rodrigo Minero Pineda on 29/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
